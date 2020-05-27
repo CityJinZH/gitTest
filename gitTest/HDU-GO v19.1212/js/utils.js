2ce92eebf07430af9b9e3047575fb653b91590ec
hi-fly <58943990+hi-fly@users.noreply.github.com>
2020-05-27 13:39:16 +0800

    Add files via upload

---
+++ gitTest/HDU-GO v19.1212/js/utils.js
@@ -0,0 +1,117 @@
+/**
+ * 一些通用方法
+ */
+
+/**
+ * 默认设置
+ * （外部脚本请勿直接访问此变量，获取配置通过 const config = await readConfig(); 进行获取）
+ */
+const OPTION = {
+	/* 抢课设置 */
+	disableCaptcha: false,
+
+	/* 自动计算学分设置 */
+	countCredit: false,
+
+	/* 自动计算学分设置 */
+	autoRate: false,
+}
+
+/**
+ * 屏蔽信息框，仅屏蔽忽略 tipList 中的提示，其他提示语按照 alert 弹出
+ * 必须在 "run_at": "document_start" 时注入才能生效
+ */
+function disableAlert() {
+	addElement(
+		'script',
+		`
+			window._alert = window.alert;
+			window.alert = (text) => {
+				if (text.includes('验证码不正确')) {
+					window._alert(text);
+					window._alert('HDU-GO 提示：请退出数字杭电账号并重新启动浏览器，使验证码屏蔽功能生效。');
+					return;
+				}
+				const tipList = ['人数超过限制'];
+				if (!tipList.some(item => text.includes(item))) {
+					window._alert(text);
+					return;
+				}
+				window.onload = () => {
+					const div = document.createElement('div');
+					div.innerHTML = text;
+					div.style = \`
+						background: #fff;
+						padding: 0 30px;
+						height: 50px;
+						line-height: 50px;
+						border-radius: 4px;
+						border: 2px solid #f00;
+						text-align: center;
+						position: fixed;
+						top: 10px;
+						right: 10px;
+					\`;
+					document.body.appendChild(div);
+				}
+			};
+		`,
+		{},
+		document.documentElement
+	);
+}
+
+/**
+ * 屏蔽验证码图片
+ */
+function disableCaptcha() {
+  const img = document.querySelector('img[src="CheckCode.aspx"]');
+  img.removeAttribute('src');
+  img.parentElement.innerHTML = 'HDU-GO 为您屏蔽了验证码，每次重新开启屏蔽验证码功能都请重新启动浏览器确保功能生效。';
+}
+
+/**
+ * 向指定 父元素 中添加 子元素
+ * @param {String} tagName 子元素标签名称
+ * @param {String} innerHTML 子元素的内容
+ * @param {Object} options 子元素的属性
+ * @param {HTMLElement} fatherEl 父元素，默认为 body
+ */
+function addElement(tagName, innerHTML = '', options = {}, fatherEl = document.body) {
+  const el = document.createElement(tagName);
+  el.innerHTML = innerHTML;
+  Object.assign(el, options);
+  fatherEl.appendChild(el);
+}
+
+/**
+ * 获取用户配置
+ * 取消回调写法，调用使用 async/await
+ */
+function readConfig() {
+	return new Promise(resolve => {
+		chrome.storage.sync.get(OPTION, function (option) {
+			// 获取到的 option 合并到内存里
+			Object.assign(OPTION, option);
+			resolve(option);
+		});
+	});
+}
+
+/**
+ * 保存用户配置
+ */
+function saveConfig(config) {
+	chrome.storage.sync.set(config);
+}
+
+/**
+ * 计算数字和，用法：sum(1, 2, 3, ...)
+ * @param  {...Number} num 待累加的数字
+ */
+function sum(...num) {
+	return num.reduce(
+		(a, b) => a + b,
+		0
+	);
+}
---
+++ gitTest/HDU-GO v19.1212/js/xf_xsqxxxk.js
@@ -0,0 +1,100 @@
+/**
+ * 网上选课 -> 通识选修课
+ * 在 http://jxgl.hdu.edu.cn/xf_xsqxxxk.aspx* 中注入本脚本
+ */
+
+'use strict';
+
+/**
+ * 当前页面抢课功能是否启用
+ * 通过 hash 进行标记
+ */
+const ENABLE = location.hash.includes('enable');
+
+// 禁用 alert 立即执行
+(function () {
+	disableAlert();
+}) ();
+
+// 入口
+window.onload = async () => {
+	const config = await readConfig();
+	config.disableCaptcha && disableCaptcha();
+	renderForm();
+	renderCtrlBtn();
+
+	if (!ENABLE) {
+		return;
+	}
+
+	if (document.querySelector('#kcmcGrid').innerHTML.length > 300) {
+		checkBox();
+		submitForm();
+	} else {
+		addLoadingBar();
+		setTimeout(() => {
+			searchClass();
+		}, 1500);
+	}
+};
+
+/**
+ * 改变表单的 action，添加页面标记
+ */
+function renderForm() {
+	document.querySelector('form[name="xsyxxxk_form"]').action += ENABLE ? '#enable' : '';
+}
+
+/**
+ * 渲染控制按钮
+ */
+function renderCtrlBtn() {
+	addElement('button', `${ENABLE ? '关闭' : '开启'}抢课模式`, {
+		onclick: function (e) {
+			// 阻止表单默认提交
+			e.preventDefault();
+
+			location.hash = ENABLE ? '' : '#enable';
+			const form = document.querySelector('form[name="xsyxxxk_form"]');
+			form.action = ENABLE ? form.action.replace('#enable', '') : `${form.action}#enable`;
+
+			// 开启抢课模式自动再搜索一次课
+			!ENABLE && searchClass();
+		}
+	}, document.querySelector('#Button2').parentElement);
+}
+
+/**
+ * 选中选课框
+ */
+function checkBox() {
+	document.querySelector('#kcmcGrid').querySelector('input').checked = true;
+}
+
+/**
+ * 提交选课
+ */
+function submitForm() {
+	document.querySelector('#Button1').click();
+}
+
+/**
+ * 添加 loading 条
+ */
+function addLoadingBar() {
+	addElement('style', `
+		@keyframes loading {
+			from { width: 0% } to { width: 100% };
+		}
+	`);
+	addElement('div', '', {
+		style: 'height: 5px; background: #cadaee; animation: loading 1.5s'
+	});
+}
+
+/**
+ * 点击搜索按钮
+ */
+function searchClass() {
+	document.querySelector('input#Button2').click();
+}
